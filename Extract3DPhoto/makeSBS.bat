4aa3f9ff4bb7159342399c231ec040ce11f428b0
Andy Modla <andy.modla@gmail.com>
2022-03-16 14:58:04 -0400

    remove pause

--- Extract3DPhoto/makeSBS.bat
+++ Extract3DPhoto/makeSBS.bat
@@ -3,4 +3,3 @@ rem full size remove -resize x1600 option
 rem resize to vertical 1600 pixels, no alignment
 rem appends _2x1 suffix for Leia LumePad 3D glasses free tablet
 magick montage %1 %2 -resize x1600 -geometry +0+0 -tile 2x1 %3_1600_2x1.jpg
-pause
